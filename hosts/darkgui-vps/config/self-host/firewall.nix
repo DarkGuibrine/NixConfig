d4872894ea6b9905249ae6601667feaca77e4927
DarkGuibrine <darkgui@protonmail.com>
2026-05-11 19:11:02 -0300

    ativar firewall

--- hosts/darkgui-vps/config/self-host/firewall.nix
+++ hosts/darkgui-vps/config/self-host/firewall.nix
@@ -9,16 +9,17 @@
 
   # Open ports in the firewall.
   networking.firewall = {
-    enable = false;
+    enable = true;
     allowedTCPPorts = [
+      22
       80
+      81
       443
+      3000
       5335
       7777
-    ];
-    allowedTCPPorts = [
-      5335
-      7777
+      9000
+      11000
     ];
     trustedInterfaces = [ "tailscale0" "docker0" ];
     allowedUDPPorts = [ config.services.tailscale.port ];
--- hosts/darkgui-vps/config/self-host/self-host.nix
+++ hosts/darkgui-vps/config/self-host/self-host.nix
@@ -16,26 +16,6 @@
     };
   };
 
-  # Open ports in the firewall.
-  networking.firewall = {
-    enable = false;
-    allowedTCPPorts = [
-      80
-      443
-    ];
-    allowedUDPPortRanges = [
-      {
-        from = 47998;
-        to = 48000;
-      }
-      {
-        from = 48002;
-        to = 48010;
-      }
-    ];
-    trustedInterfaces = [ "tailscale0" ];
-    allowedUDPPorts = [ config.services.tailscale.port ];
-  };
 
   boot.kernel.sysctl = {
     "net.ipv4.ip_unprivileged_port_start" = 53;
