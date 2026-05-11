ecd5523a9487aca271c8eeb459f4ffdb1f0b0207
DarkGuibrine <darkgui@protonmail.com>
2026-05-11 19:30:15 -0300

    Update firewall.nix

--- hosts/darkgui-vps/config/self-host/firewall.nix
+++ hosts/darkgui-vps/config/self-host/firewall.nix
@@ -6,7 +6,6 @@
   ...
 }:
 {
-
   # Open ports in the firewall.
   networking.firewall = {
     enable = true;
@@ -21,8 +20,20 @@
       9000
       11000
     ];
+
     trustedInterfaces = [ "tailscale0" "docker0" ];
-    allowedUDPPorts = [ config.services.tailscale.port ];
+    allowedUDPPorts = [
+      config.services.tailscale.port
+      53
+      5300
+      7777
+      40000
+    ];
+    extraCommands = ''
+      iptables -A PREROUTING -t nat -i eth0 -p TCP --dport 80 -j REDIRECT --to-port 8000
+      iptables -A PREROUTING -t nat -i eth0 -p TCP --dport 53 -j REDIRECT --to-port 5300
+      iptables -A PREROUTING -t nat -i eth0 -p UDP --dport 53 -j REDIRECT --to-port 5300
+    '';
   };
 
 }
